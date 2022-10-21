148973efc42669b6a4feebf81ff429aadc0a2ffc
ilakalasnikov <753476Kk>
2022-10-21 12:12:21 +0300

    Spring Cloud Gateway added, gateway routing is enabled

--- organization-microservice/src/main/java/org/gisbis/organization/microservice/controller/DefaultController.java
+++ organization-microservice/src/main/java/org/gisbis/organization/microservice/controller/DefaultController.java
@@ -1,12 +1,15 @@
 package org.gisbis.organization.microservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.gisbis.organization.microservice.model.Organization;
 import org.gisbis.organization.microservice.model.User;
 import org.gisbis.organization.microservice.service.OrganizationService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -17,5 +20,10 @@ public class DefaultController {
     @GetMapping("/{userId}")
     public User getUserById(@PathVariable("userId") int userId){
         return organizationService.getUserById(userId);
+    }
+
+    @GetMapping("/organizations")
+    public List<Organization> getOrganizations(){
+        return organizationService.getOrganizations();
     }
 }
--- organization-microservice/src/main/java/org/gisbis/organization/microservice/service/OrganizationService.java
+++ organization-microservice/src/main/java/org/gisbis/organization/microservice/service/OrganizationService.java
@@ -1,17 +1,30 @@
 package org.gisbis.organization.microservice.service;
 
+import org.gisbis.organization.microservice.model.Organization;
 import org.gisbis.organization.microservice.model.User;
+import org.gisbis.organization.microservice.repository.OrganizationRepo;
 import org.gisbis.organization.microservice.service.client.UserRestTemplateClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrganizationService {
     @Autowired
     UserRestTemplateClient restTemplateClient;
+    private final OrganizationRepo organizationRepo;
+
+    public OrganizationService(OrganizationRepo organizationRepo) {
+        this.organizationRepo = organizationRepo;
+    }
 
     public User getUserById(int userId){
         return restTemplateClient.getUser(userId);
+    }
+
+    public List<Organization> getOrganizations(){
+        return organizationRepo.findAll();
     }
 
 }
